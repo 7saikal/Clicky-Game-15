26501b884999c6e15f370a5c614da5c66e67779c
Saikal Asylbekova <saikalasylbekova@Saikals-MacBook-Pro.local>
2019-11-14 18:26:19 -0600

    clickygame

---
+++ client/images.js
@@ -0,0 +1,29 @@
+import angelina from './images/angelina.jpg';
+import bill from './images/bill.jpg';
+import brad from './images/brad.jpg';
+import elon from './images/elon.jpg';
+import marc from './images/marc.jpg';
+import michael from './images/michael.jpg';
+import michelle from './images/michelle.jpg';
+import oprah from './images/oprah.jpg';
+import tony from './images/tony.jpg';
+import song from './images/song.jpg';
+import lee from './images/lee.jpg';
+import obama from './images/obama.jpg';
+
+const images = [
+  angelina,
+  bill,
+  brad,
+  elon,
+  marc,
+  michael,
+  michelle,
+  oprah,
+  tony,
+  song,
+  lee,
+  obama
+];
+
+export default images;
